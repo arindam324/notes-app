936f92304fea7eccd85628ddc803eb3aa9dfdc6d
arindam234 <aroy92647@gmal.com>
2022-09-09 00:34:25 +0530

    add dark theme on appbar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,23 @@
 import React, {ReactElement} from "react";
 
 import {PencilSquareIcon} from '@heroicons/react/24/outline'
+import {Button, useMantineColorScheme} from '@mantine/core'
+
 
 const Sidebar = () => {
+    const {colorScheme, toggleColorScheme} = useMantineColorScheme();
     return (
-        <aside className={"max-w-[250px]  w-full min-h-screen bg-gray-200 flex flex-col py-4"}>
-            <NavLink Icon={<PencilSquareIcon className={"h-6"}/>} name={"Notes"} isActive/>
+        <aside
+            className={`max-w-[250px]  w-full min-h-screen bg-gray-200 flex flex-col py-4 justify-between ${colorScheme === 'dark' ? 'bg-[#1a2421] text-white' : ""} `}>
+            <nav>
+                <NavLink Icon={<PencilSquareIcon className={"h-6"}/>} name={"Notes"} isActive/>
+            </nav>
+
+            <div className={"sel"}>
+                <Button variant={"subtle"} color={"dark"} onClick={() => toggleColorScheme()}>
+                    {colorScheme === "dark" ? "Light Theme" : "Dark Theme"}
+                </Button>
+            </div>
         </aside>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,31 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 
-import {MantineProvider} from '@mantine/core'
+import {ColorSchemeProvider, MantineProvider, ColorScheme} from '@mantine/core'
+import {useLocalStorage} from "@mantine/hooks";
+
 
 function MyApp({Component, pageProps}: AppProps) {
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: 'mantine-color-scheme',
+        defaultValue: 'light',
+        getInitialValueInEffect: true
+    });
+
+    const toggleColorScheme = (value?: ColorScheme) => {
+        setColorScheme(value || (colorScheme === "dark" ? 'light' : 'dark'))
+    }
+
     return (
-        <MantineProvider
-            withGlobalStyles
-            theme={{
-            colorScheme:'light'
-        }}>
-            <Component {...pageProps} />
-        </MantineProvider>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider
+                withGlobalStyles
+                theme={{
+                    colorScheme: 'light'
+                }}>
+                <Component {...pageProps} />
+            </MantineProvider>
+        </ColorSchemeProvider>
     )
 }
 
