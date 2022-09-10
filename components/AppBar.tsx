5ca7eb3260755765829a441a2383e665274ece46
arindam234 <aroy92647@gmal.com>
2022-09-11 01:36:59 +0530

    added a modal for creating new notes

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,6 +1,6 @@
-import React, {useMemo} from "react";
+import React, {useMemo, useState} from "react";
 import {MagnifyingGlassIcon, PlusIcon} from '@heroicons/react/24/outline'
-import {ScrollArea, useMantineColorScheme} from '@mantine/core'
+import {Modal, ScrollArea, useMantineColorScheme, Input, Textarea, Button} from '@mantine/core'
 import {useEditor} from "../Context/EditorProvider";
 
 const Data = [
@@ -40,14 +40,35 @@ const Data = [
 
 const AppBar = () => {
     const {colorScheme} = useMantineColorScheme()
+    const [opened, setOpen] = useState(false)
+
+    const theme = useMantineColorScheme()
+
     return (
         <div
             className={`max-w-[450px]  pt-4 border-r w-full min-h-screen ${colorScheme === 'dark' ? "bg-black text-white" : ""} `}>
+            <Modal
+                opened={opened}
+                overlayColor={theme.colorScheme === 'dark' ? "#101113" : "#909296"}
+                overlayOpacity={0.55}
+                overlayBlur={3}
+                onClose={() => setOpen(false)}
+                title={"create a new Notes"}
+            >
+                <Input
+                    placeholder="enter your notes name"
+                />
+                <Textarea
+                    my={"xs"}
+                    placeholder={"add a little description for your notes"}
+                />
+                <button className={"px-10 py-2  rounded-lg bg-black text-white"}>Submit</button>
+            </Modal>
             <div className={"flex items-center px-6 justify-between"}>
                 <h2 className={"text-2xl font-semibold"}>Notes</h2>
                 <div className={"flex space-x-4"}>
                     <MagnifyingGlassIcon className={"h-6"}/>
-                    <PlusIcon className={"h-6"}/>
+                    <PlusIcon onClick={() => setOpen(true)} className={"h-6 cursor-pointer"}/>
                 </div>
             </div>
             <div className={"w-full space-y-2"}>
